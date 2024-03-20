1b1935c43dfa9dac94327a225c26e95cc8a2022d
Bárbara_Bueno <65515809+Buenobarbie@users.noreply.github.com>
2024-03-20 10:13:03 -0300

    :bug: Fix display J

--- code/componentes/hexa7seg_J.v
+++ code/componentes/hexa7seg_J.v
@@ -6,8 +6,22 @@ module hexa7seg_J (habilita_J, J);
    * input habilita_J deve ser 1'b1 no circuito
    */
 
+   
+    /*
+     *    ---
+     *   | 0 |
+     * 5 |   | 1
+     *   |   |
+     *    ---
+     *   | 6 |
+     * 4 |   | 2
+     *   |   |
+     *    ---
+     *     3
+     */
+
     always @ (habilita_J) begin
-        if(habilita_J) J = 7'b0001110; // J 
+        if(habilita_J) J = 7'b1100001; // J 
         else J = 7'b0111111; // - 
     end
 endmodule
