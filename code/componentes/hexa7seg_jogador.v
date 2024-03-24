7f54a575e3f7b2308dce3ce590a2814fe569dbe4
Tássyla Lima <56099401+tassyla@users.noreply.github.com>
2024-03-24 10:51:45 -0300

    componentes

--- code/componentes/hexa7seg_jogador.v
+++ code/componentes/hexa7seg_jogador.v
@@ -6,11 +6,24 @@ module hexa7seg_jogador (jogador, display);
    * se jogador = 0, display = 1
    * se jogador = 1, display = 2
    */
+   
+    /*
+     *    ---
+     *   | 0 |
+     * 5 |   | 1
+     *   |   |
+     *    ---
+     *   | 6 |
+     * 4 |   | 2
+     *   |   |
+     *    ---
+     *     3
+     */
 
     always @ (jogador) 
         case(jogador)
-            1'b0:    display = 7'b0100100; // 1
-            1'b1:    display = 7'b1111001; // 2
+            1'b0:    display = 7'b1111001; // 1
+            1'b1:    display = 7'b0100100; // 2
 
             default: display = 7'b0111111; // -
         endcase
